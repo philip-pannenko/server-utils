7701dc63fc1c355e5effc5c461ac708b264ced32
Philip Pannenko <philip.pannenko@gmail.com>
2019-01-14 23:20:24 -0500

    Initial commit.

---
+++ src/main/java/net/pannenko/cicd/MessageUtil.java
@@ -0,0 +1,9 @@
+package net.pannenko.cicd;
+
+public class MessageUtil {
+
+    public static String toUpperCase(String message) {
+        return message == null ? message : message.toUpperCase();
+    }
+
+}
